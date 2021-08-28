4053b57b4aaeebe191216360a8d2c8a23ca6f738
unknown <manikdae34.com>
2021-08-28 13:22:05 +0600

    localhost and dae website included

--- schools/settings.py
+++ schools/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^kr_#8u8i^lz_w&eqa+nbj#122p=bctn3#s850wxdk9y#cnvfr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['20.204.23.237']
+ALLOWED_HOSTS = ['20.204.23.237', '127.0.0.1']
 
 
 # Application definition
